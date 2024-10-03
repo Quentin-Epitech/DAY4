541c410026a619cf714149832f142e6b408aae24
Quentin-Epitech <quentin.bonnet@epitech.digital>
2024-10-03 13:34:24 +0200

    6eme ex

---
+++ resources_day05/ex_06/ex_06.js
@@ -0,0 +1,9 @@
+window.onresize = function() {
+    alert("Size matters");};
+window.onload = function() {
+    const Image = document.querySelector("img[src='../Iphone22.png']");
+    Image.addEventListener("mouseover", function() {
+        alert("Pretty, isn't it?");
+    });};
+
+    
